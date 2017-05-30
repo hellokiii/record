7fa2d04b0c5d70ddd88c99b365512981c3f7fb3a
hellokiii <hellokiii@naver.com>
2017-05-30 06:43:14 +0000

    5이닝, 1000회 평균 추가

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
 
+  
   @@advance = {"baseonballs": [1],
              "hbp": [1],
              "single": [0,1],
@@ -8,6 +9,11 @@ class HomeController < ApplicationController
              "homerun": [1,0,0,0],
              "pb": [0]
             }
+  @@inning = 1
+  @@batter_num = 0
+  @@result = Array.new
+  @@juja = Array.new
+  @@run = 0
 
   def index
     simulation
@@ -18,32 +24,71 @@ class HomeController < ApplicationController
     redirect_to '/home/index', notice: "완료!"
   end
   
+  def simul
+    @@result = []
+    @@juja = []
+    @@inning = 1
+    @@batter_num = 0
+    redirect_to '/home/simulation'
+  end
+  
   def simulation
     out = 0
-    batter_num = 0
-    juja = Array.new
-    @result = Array.new
+    @@result << @@inning.to_s + "이닝"
     while out < 3
       Record.all.each do |r|
-        batter_num += 1
-        batter_num = 1 if batter_num > 9
+        @@batter_num += 1
+        @@batter_num = 1 if @@batter_num > 9
         result_of_the_batter = r.batter_result
-        @result << batter_num.to_s + "번타자 " + Record.find(batter_num).name + ": " + result_of_the_batter
+        @@result << @@batter_num.to_s + "번타자 " + Record.find(@@batter_num).name + ": " + result_of_the_batter
         if result_of_the_batter == "strikeout"
           out += 1
         elsif result_of_the_batter == "pb"
           out += 1
-          break if out == 3
-          juja += @@advance[result_of_the_batter.to_sym]
+          if out == 3
+            @@inning += 1
+            break
+          end
+          @@juja += @@advance[result_of_the_batter.to_sym]
         else
-          juja += @@advance[result_of_the_batter.to_sym]
+          @@juja += @@advance[result_of_the_batter.to_sym]
         end
-        break if out == 3
+        if out == 3
+          @@inning += 1
+          break
+        end
       end
     end
-    @juja = juja
-    leftonbase = juja.last(3)
-    @scorerunner = juja.count(1) - leftonbase.count(1)
+    @juja = @@juja
+    @result = @@result
+    leftonbase = @@juja.last(3)
+    @scorerunner = @@juja.count(1) - leftonbase.count(1)
+  end
+  
+  def five_inning
+    @@result = []
+    5.times do
+      simulation
+      @@result << @scorerunner.to_s + " 점"
+      @@result << ""
+      @@run += @scorerunner
+      @@juja = []
+    end
+      @result = @@result
+      @run = @@run
+      @@inning = 1
+      @@batter_num = 0
+      @@run = 0
+  end
+  
+  
+  def average
+    @sum = 0.to_f
+    1000.times do
+      five_inning
+      @sum += @run
+    end
     
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   post 'home/upload'
   get 'home/table'
   get 'home/simulation'
+  get 'home/five_inning'
+  get 'home/simul'
+  get 'home/average'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
